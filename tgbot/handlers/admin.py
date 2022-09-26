9e9b20f06d25db4158a996e99b134184b17736a9
deitydivan <83013819+deitydivan@users.noreply.github.com>
2022-09-26 03:44:48 +0300

    fix import handlers

--- tgbot/handlers/admin.py
+++ tgbot/handlers/admin.py
@@ -1,10 +1,19 @@
-from aiogram import Router
-from aiogram.types import Message
+from aiogram import Router, Bot, types
+from aiogram.types import Message, FSInputFile
+from tgbot.config import load_config
+from aiogram.dispatcher.fsm.context import FSMContext
+from aiogram.dispatcher.fsm.state import State, StatesGroup
+
+import psycopg2
+from psycopg2 import sql
+from psycopg2.extensions import AsIs
 
 from tgbot.filters.admin import AdminFilter
 
 admin_router = Router()
 admin_router.message.filter(AdminFilter())
+config = load_config(".env")
+bot = Bot(token=config.tg_bot.token, parse_mode='HTML')
 
 
 # @admin_router.message(commands=["start"], state="*")
--- tgbot/handlers/user.py
+++ tgbot/handlers/user.py
@@ -1,7 +1,16 @@
-from aiogram import Router
-from aiogram.types import Message
+from aiogram import Router, Bot, types
+from aiogram.types import Message,FSInputFile
+from tgbot.config import load_config
+from aiogram.dispatcher.fsm.context import FSMContext
+from aiogram.dispatcher.fsm.state import State, StatesGroup
+
+import psycopg2
+from psycopg2 import sql
+from psycopg2.extensions import AsIs
 
 user_router = Router()
+config = load_config(".env")
+bot = Bot(token=config.tg_bot.token, parse_mode='HTML')
 
 
 @user_router.message(commands=["start"])
