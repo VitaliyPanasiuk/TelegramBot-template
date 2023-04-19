308ba63f7502ef6923e54d3724db6daf16617ec8
Vitaliy <panasyukdeveloper@gmail.com>
2023-04-19 21:51:13 +0300

    fix fsm import

--- tgbot/misc/functions.py
+++ tgbot/misc/functions.py
@@ -1,8 +1,8 @@
 from aiogram import Router, Bot, types
 from aiogram.types import Message, FSInputFile
 from tgbot.config import load_config
-from aiogram.dispatcher.fsm.context import FSMContext
-from aiogram.dispatcher.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
 
 import psycopg2
 from psycopg2 import sql
--- tgbot/misc/states.py
+++ tgbot/misc/states.py
@@ -1,5 +1,5 @@
-from aiogram.dispatcher.fsm.context import FSMContext
-from aiogram.dispatcher.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
 
 
 class exmple_state(StatesGroup):
